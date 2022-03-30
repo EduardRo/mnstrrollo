39ea936ebbe9d8e8daf1dbae09089e37be51b394
EduardRo <eduardstancu@yahoo.com>
2022-03-30 10:57:41 +0300

    mod_11

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
+import {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+class App extends Component{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+constructor(){
+  super();
+  this.state = {
+    name:'Andre'
+  }
+  
 }
+
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            My name is {this.state.name}
+          </p>
+          <button>hello my name is E</button>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+
+}
+
 
 export default App;
