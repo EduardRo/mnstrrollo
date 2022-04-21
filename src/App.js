71e01349b18f290e587a327d0575f31352df1626
EduardRo <eduardstancu@yahoo.com>
2022-04-21 14:32:29 +0300

    mod38

--- src/App.js
+++ src/App.js
@@ -40,16 +40,28 @@ response.json())
   render(){
     return (
       <div className="App" key='123'>
+        <input className='search-box' type='search' placeholder='search monster' 
+        onChange={(event)=>{
+         const searchString = event.target.value.toLocaleLowerCase();
+         const filteredMonsters = this.state.monsters.filter((monster)=>{
+          return monster.name.toLocaleLowerCase().includes(searchString);
+         });
+         this.setState(()=>{return {monsters:filteredMonsters};}
+         );
+         
+
+        }}
+        />
         
         {this.state.monsters.map((data)=>{
           return <div key={data.id}>
            <h1>{data.name}</h1> 
            <p>{data.email}</p>
-           <>
+           <div>
            Adress:
            <p>street: {data.address.street}, suite: {data.address.suite}</p>
            <p>company: {data.company.name}</p>
-           </>
+           </div>
           
             
             </div>;
