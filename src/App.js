a6a56cdde709f73ffbc48e22136f008f76737078
EduardRo <eduardstancu@yahoo.com>
2022-04-25 10:10:37 +0300

    mod42_1

--- src/App.js
+++ src/App.js
@@ -42,14 +42,17 @@ onSearchChanga=(event)=>{
 
 
   render(){
-    const filteredMonsters = this.state.monsters.filter((monster)=>{
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);});
+
+    const {monsters, searchField} = this.state;
+    const {onSearchChange} = this;
+    const filteredMonsters = monsters.filter((monster)=>{
+      return monster.name.toLocaleLowerCase().includes(searchField);});
 
 
     return (
       <div className="App" key='123'>
         <input className='search-box' type='search' placeholder='search monster' 
-        onChange={this.onSearchChange}
+        onChange={onSearchChange}
         />
         
         {filteredMonsters.map((monster)=>{
